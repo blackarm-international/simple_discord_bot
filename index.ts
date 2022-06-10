1e2f18d4366e85e1bfbd3f448592d9bb34ff992a
brixton <brixton@blackarm-international.com>
2022-06-10 11:00:50 +0100

    more deps

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const botData = require('./config.json');
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
